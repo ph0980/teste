a493c9421b43f300298c7522a94374981acd52ae
ph0980 <pedrohenrique009800@gmail.com>
2018-10-25 15:50:55 -0300

    'teste'

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+
+def noticias(request):
+    return render(request,'noticias.html')
+
+def listaCursos(request):
+    return render(request,'listaCursos.html')
+
+def disciplina(request):
+    return render(request,'disciplina.html')
+
+def detalheCurso(request):
+    return render(request,'detalheCurso.html')
+# Create your views here.
