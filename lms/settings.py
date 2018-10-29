154061c686b22dfdab8af71a4f13f819d2f7c1df
Pedro Henrique <38732869+ph0980@users.noreply.github.com>
2018-10-29 09:12:53 -0300

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(CORE/STATIC,'static')
+STATIC_ROOT = os.path.join(STATIC,'static').replace('\\','/')
+STATICFILES_DIRS = ()
