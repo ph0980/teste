293f95b4b55212f5bb39103bb0b61712b7f1effa
Pedro Henrique <38732869+ph0980@users.noreply.github.com>
2018-10-29 08:56:02 -0300

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u1u5)2iur-g-6kyz522a3eg5q_@3c0d!5+3$=a72hu0sq8c$31'
 DEBUG = True
 
 #alterei isso aqui
-ALLOWED_HOSTS = ['lms-proj-2018.azurewebsites.net']
+ALLOWED_HOSTS = ['teste-django-milagre.azurewebsites.net']
 #ALLOWED_HOSTS = []
 
 
