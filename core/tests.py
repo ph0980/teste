7899b9c02ddd68723173281188c72cc67b7904d3
ph0980 <pedrohenrique009800@gmail.com>
2018-10-31 16:24:33 -0300

    teste adicionado

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.shortcuts import render
+from .views import *
 
 # Create your tests here.
+test__index():
+    assert index('index') == render(request,'index.html')
+    
