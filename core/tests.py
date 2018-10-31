3cb158437a6dda66b3fbd02cc48d7f271bdbf9f4
ph0980 <pedrohenrique009800@gmail.com>
2018-10-31 16:40:26 -0300

    vacilo

--- core/tests.py
+++ core/tests.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from .views import *
 
 # Create your tests here.
-test__index():
-    request='index'
-    assert index(request) == render(request,'index.html')
-
+def test__index():
+    assert index('index') == render(request,'index.html')
+    
